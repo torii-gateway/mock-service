fdd1cafe1240e85be13969c2b551ca6532a3d294
g-terra <by.terra@hotmail.com>
2023-06-15 15:33:52 +0200

    improvements and bug fixes

--- src/main/java/org/torii/gateway/mockservice/controller/EchoController.java
+++ src/main/java/org/torii/gateway/mockservice/controller/EchoController.java
@@ -1,5 +1,7 @@
 package org.torii.gateway.mockservice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/echo")
 public class EchoController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping
     public ResponseEntity echo() {
-        return ResponseEntity.ok().header("X-Test-Header", "test").build();
+        //delays the response by .4 seconds
+
+        try {
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok().header("X-instance-name", environment.getProperty("instance.name")).build();
     }
 }
